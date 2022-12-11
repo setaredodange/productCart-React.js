4a76977b39f23105894ccb3dd5e170ce4926b753
somayehdodangeh <dodangeh7019@gmail.com>
2022-12-11 11:24:05 -0800

    spread syntax done

--- my/src/App.js
+++ my/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Product from './Product/Product'
 
 function App() {
+
+  const allProducts=[
+    {id:1, img:"download1.jpg", name:"iPhone14",  price:1200 , content:"it is the best and new phone for 2022", count:2},
+    {id:2, img:"download4.jpg", name:"iPhone13",  price:1100, content:"smart and beautiful color", count:5},
+    {id:3, img:"images3.jfif", name:"iPhone12",  price:900, content:"it is the best  for 2020", count:3 }
+   
+  ]
   return (
     <div className="container">
-    <Product img="download1.jpg" name="iPhone14"  price={1200} content="it is the best and new phone for 2022" count={2}/>
-    <Product img="download4.jpg" name="iPhone13"  price={1100} content="smart and beautiful color" count={5}/>
-    <Product img="images3.jfif" name="iPhone12"  price={900} content="it is the best  for 2020" count={0}/>
+    <Product {...allProducts[0]} />
+    <Product {...allProducts[1]} />
+    <Product {...allProducts[2]} />
 
     </div>
   );
